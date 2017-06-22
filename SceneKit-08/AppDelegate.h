1350b53494ccf2826db43c6101776daf38ba595e
Chenshiwen <cswzmg@live.com>
2017-06-22 11:47:15 +0800

    Initial Commit

---
+++ SceneKit-08/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SceneKit-08
+//
+//  Created by ShiWen on 2017/6/22.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
